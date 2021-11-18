27294e112220b3cc2777715b685b65c4e46b250c
Gabriel-Dombach <gdombach009@gmail.com>
2021-11-18 09:11:16 -0500

    got image upload working

--- manager-page/script.js
+++ manager-page/script.js
@@ -1,19 +1,23 @@
-let arr1 = [];
-if (localStorage.getItem("menue") != null) {
-  arr1 = JSON.parse(localStorage.getItem("menue"));
-}
 $(document).ready(function () {
-  $("#upload").on("click", function () {
-    var file_data = $("#file").prop("files")[0];
-    var form_data = new FormData();
-    form_data.append("file", file_data);
-    const newItem = {
-      image: form_data,
-      itemName: $(".nameNew").val(),
-      itemCost: $(".costNew").val(),
-    };
+  const image_input = document.querySelector("#file");
+  var uploaded_image = "";
+  image_input.addEventListener("change", function () {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      uploaded_image = reader.result;
+    });
+  });
+  let arr1 = [];
+  if (localStorage.getItem("menue") != null) {
     arr1 = JSON.parse(localStorage.getItem("menue"));
-    arr1.push(newItem);
+  }
+  $("#upload").on("click", function () {
+    const newItem = {
+      image: uploaded_image,
+      name: $(".nameNew").val(),
+      price: $("costNew").val(),
+    };
+    arr1.push(JSON.stringify(newItem));
     localStorage.setItem("menue", JSON.stringify(arr1));
   });
 });
