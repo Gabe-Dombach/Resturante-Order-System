59abc093c4110c01431d25f272f1a1acd5854dd3
EBarrett233 <ekbarrett211@stevenscollege.edu>
2021-12-02 10:59:49 -0500

    work

--- recipt-page/script.js
+++ recipt-page/script.js
@@ -17,16 +17,3 @@ $(document).ready(function(){
   })
 }) 
 
-for (let i = 0; i < reciept.length; i++) {
-  let currObj = JSON.parse(reciept[i]);
-  //total = parseInt(currObj.cost)
-  let price = currObj.cost 
-  var finalPrice = price.replace(/\$/g,'')
-  var num = parseInt(finalPrice,10)
-  total = num + i
-  $("#items").append('<tr><td><p>Item:' + currObj.name + '<P></P>'+'Cost:' + currObj.cost + '</p></td></tr>')
-  console.log(total)
-}
-
-document.getElementById("total").innerHTML = "Total: $" + total
-localStorage.setItem("total", JSON.stringify(total));
