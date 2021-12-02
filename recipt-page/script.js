a08ebe2d190d9a4c5f98a0d2a297f8478b863f29
EBarrett233 <ekbarrett211@stevenscollege.edu>
2021-12-02 11:23:41 -0500

    upgrade

--- recipt-page/script.js
+++ recipt-page/script.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
   let reciept = JSON.parse(localStorage.getItem("reciept"))
-  console.log (reciept)
+  console.log(reciept)
+
+  for (let i = 0; i < reciept.length; i++) {
+    let currObj = JSON.parse(reciept[i]);
+
+    $("#items").append('<tr><td><p>Item:' + currObj.name + '<P></P>'+'Cost:' + currObj.cost + '</p></td></tr>')
+    console.log(total)
+  }
+
   
   $('send').click(function(){
       Email.send({
