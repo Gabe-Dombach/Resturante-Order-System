ae605a2a2fb8ca2fffc841d69413dac908bce8b0
Gabriel-Dombach <gdombach009@gmail.com>
2021-12-06 11:05:31 -0500

    fixed project

--- menue/script.js
+++ menue/script.js
@@ -1,19 +1,18 @@
-let list = []
+let list = [];
 
-$(document).ready(function()//adds items to the menu on page load
+$(document).ready(function () //adds items to the menu on page load
 {
-  list = JSON.parse(localStorage.getItem("menue"))
-  for(let i = 0; i < list.length; i++)
-  {
-    list[i] = JSON.parse(list[i])
-    let foodId = list[i].idName
-    foodId = foodId.join("")
-    let food = list[i].name
-    let imageData = list[i].image
-    let price = list[i].price //lines 3-13 parse out the jsons array and then parse out items from the array
+  list = JSON.parse(localStorage.getItem("menue"));
+  for (let i = 0; i < list.length; i++) {
+    list[i] = JSON.parse(list[i]);
+    let foodId = list[i].idName;
+    foodId = foodId.join("");
+    let food = list[i].name;
+    let imageData = list[i].image;
+    let price = list[i].price; //lines 3-13 parse out the jsons array and then parse out items from the array
     $("#food").append(
       "<tr><td id=" +
-        foodId + 
+        foodId +
         "0>" +
         "<img src=" +
         imageData +
@@ -30,8 +29,8 @@ $(document).ready(function()//adds items to the menu on page load
   }
 });
 
-function addItem(a) //adds items to the order
- {
+function addItem(a) {
+  //adds items to the order
   let item = a.id;
   let value = "#" + item + 1;
   let image = "#" + item + 0;
@@ -39,7 +38,8 @@ function addItem(a) //adds items to the order
   let valueContent = $(value).html();
   let checker = $("#1" + item).html();
   var count = $(a).data("count") || 0; //lines 35-41 grab all the data fron the item you wish to add and formats them for the new element
-  if (count < 1) { //this if statement runs if the item is not on the order, as tracked by the "count" data attribute attached to each element
+  if (count < 1) {
+    //this if statement runs if the item is not on the order, as tracked by the "count" data attribute attached to each element
     $(a).data("count", ++count);
     $("#items").append(
       "<tr id=1" +
@@ -52,12 +52,12 @@ function addItem(a) //adds items to the order
         count +
         '</div></td><td><button onclick="removeItem(this)" id=but' +
         item +
-        ">Remove</button></td></tr>"//lines 45-57 construct the item in the order table
+        ">Remove</button></td></tr>" //lines 45-57 construct the item in the order table
     );
-  } 
-  else if(count > 1 && checker == undefined){//this if statement runs if the item has been previously added to the order table, but has since been removed, it just resets the "count" attribute
+  } else if (count > 1 && checker == undefined) {
+    //this if statement runs if the item has been previously added to the order table, but has since been removed, it just resets the "count" attribute
     $(a).data("count", 1);
-    let count = 1
+    let count = 1;
     $("#items").append(
       "<tr id=1" +
         item +
@@ -71,13 +71,13 @@ function addItem(a) //adds items to the order
         item +
         ">Remove</button></td></tr>"
     );
-  } 
-  else { //if niether the previous two statements run, that means that this item is already in the order table, so this statement increase the quantity of the item and changes the item cost to reflect this
+  } else {
+    //if niether the previous two statements run, that means that this item is already in the order table, so this statement increase the quantity of the item and changes the item cost to reflect this
     $("#1" + item).remove();
     valueContent = valueContent.split("");
     valueContent.shift();
     $(a).data("count", ++count);
-    valueContent = valueContent.join("")
+    valueContent = valueContent.join("");
     valueContent = Number(valueContent);
     valueContent = valueContent * count;
     valueContent = valueContent.toFixed(2);
@@ -98,79 +98,68 @@ function addItem(a) //adds items to the order
   }
 }
 
-function checkout() //this is creates the json array and puts it into local storage
-{
-  if($("#items").text() != undefined)//the if statement ensures that the function only operatesif there is something in the order to avoid potential errors and because you can't pay for nothing
-  {
-  let arr1 = []
-  var table = document.getElementById("items");
-  for (let i = 0, row; row = table.rows[i]; i++) {
-    let itemName = ""
-    let total = ""
-    let quantity = ""
-    for (let j = 0, col; col = row.cells[j]; j++) {
-      if(j == 1)
-      {
-        let temp = col.innerHTML
-        temp = temp.split("")
-        for(let d = 0; d < 5; d++)
-        {
-          temp.shift()
+function checkout() {
+  //this is creates the json array and puts it into local storage
+  if ($("#items").text() != undefined) {
+    //the if statement ensures that the function only operatesif there is something in the order to avoid potential errors and because you can't pay for nothing
+    let arr1 = [];
+    var table = document.getElementById("items");
+    for (let i = 0, row; (row = table.rows[i]); i++) {
+      let itemName = "";
+      let total = "";
+      let quantity = "";
+      for (let j = 0, col; (col = row.cells[j]); j++) {
+        if (j == 1) {
+          let temp = col.innerHTML;
+          temp = temp.split("");
+          for (let d = 0; d < 5; d++) {
+            temp.shift();
+          }
+          for (let d = 0; d < 6; d++) {
+            temp.pop();
+          }
+          itemName = temp.join("");
+        } else if (j == 2) {
+          let temp = col.innerHTML;
+          temp = temp.split("");
+          for (let d = 0; d < 5; d++) {
+            temp.shift();
+          }
+          for (let d = 0; d < 6; d++) {
+            temp.pop();
+          }
+          total = temp.join("");
+        } else if (j == 3) {
+          let temp = col.innerHTML;
+          temp = temp.split("");
+          for (let d = 0; d < 5; d++) {
+            temp.shift();
+          }
+          for (let d = 0; d < 6; d++) {
+            temp.pop();
+          }
+          quantity = temp.join("");
         }
-        for(let d = 0; d < 6; d++)
-        {
-          temp.pop()
-        }
-        itemName = temp.join("")
       }
-      else if(j == 2)
-      {
-        let temp = col.innerHTML
-        temp = temp.split("")
-        for(let d = 0; d < 5; d++)
-        {
-          temp.shift()
-        }
-        for(let d = 0; d < 6; d++)
-        {
-          temp.pop()
-        }
-        total = temp.join("")
-      }
-      else if(j == 3)
-      {
-        let temp = col.innerHTML
-        temp = temp.split("")
-        for(let d = 0; d < 5; d++)
-        {
-          temp.shift()
-        }
-        for(let d = 0; d < 6; d++)
-        {
-          temp.pop()
-        }
-          quantity = temp.join("")
-      }
+      const newItem = {
+        name: itemName,
+        cost: total,
+        amount: quantity,
+      };
+      arr1.push(JSON.stringify(newItem));
+      localStorage.setItem("reciept", JSON.stringify(arr1));
     }
-    const newItem = {
-      name: itemName,
-      cost: total,
-      amount: quantity
-    };
-    arr1.push(JSON.stringify(newItem));
-    localStorage.setItem("reciept", JSON.stringify(arr1));
-  }
-  window.location.href = '../payment/payment.html'
+    window.location.href = "../payment/payment.html";
   }
 }
 
-function removeItem(a)// removes the item attached to the button from the cart
-{
-    let item = a.id;
-    item = item.split("")
-    item.shift()
-    item.shift()
-    item.shift()
-    item = item.join("")
-    $("#1" + item).remove();
+function removeItem(a) {
+  // removes the item attached to the button from the cart
+  let item = a.id;
+  item = item.split("");
+  item.shift();
+  item.shift();
+  item.shift();
+  item = item.join("");
+  $("#1" + item).remove();
 }
